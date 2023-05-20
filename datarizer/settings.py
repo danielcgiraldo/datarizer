aac8452cdf7b3f34bcd124268ad7c27e4e4e7145
Daniel Castillo <dc.giraldocg@hotmail.com>
2023-05-19 21:58:45 -0500

    static files

--- datarizer/settings.py
+++ datarizer/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,7 +116,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
